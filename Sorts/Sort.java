b237f4a767bb7c302737d26bcca182cc6da16a62
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-02-23 20:49:32 -0600

    Compare

--- Sorts/Sort.java
+++ Sorts/Sort.java
@@ -1,3 +1,19 @@
 package Sorts;
 
-public class Sort implements IComparator{}
+public class Sort implements IComparator <Integer>{
+   
+    @Override
+    public int Compare(Integer x, Integer y ){
+
+        int res;
+
+        if (x>y) {
+            res = 0;
+        }else if (x<y){
+            res = 1;
+        }else {
+            res = -1;
+        }
+        return res;
+    }
+}
