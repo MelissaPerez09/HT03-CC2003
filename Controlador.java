b0d39eb74fd76344e08b1c2bde35e9ad442e2c6b
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-02-23 21:04:18 -0600

    gnome

--- Controlador.java
+++ Controlador.java
@@ -1,1 +1,10 @@
+/**
+ * Universidad del Valle de Guatemala
+ * @author Jimena Hernández/21199 Mark Albrand/21004 Emily Pérez/21385
+ * @version 25/02/2022
+ * Algoritmos y estructuras de Datos HDT3
+ * 
+ * Controlador.java 
+ * main del programa
+ */
 public class Controlador{}
--- Sorts/Gnome.java
+++ Sorts/Gnome.java
@@ -1,5 +1,30 @@
 package Sorts;
 
 public class Gnome extends Sort {
+
+    /**
+     * 
+     * @param datos
+     * @param size
+     */
+    public void gnomeSort(int datos[], int size)
+    {
+        int index = 0;
+ 
+        while (index < size) {
+            if (index == 0)
+                index++;
+            if (Compare(datos[index], datos[index - 1]) == 0 || Compare(datos[index], datos[index - 1]) == -1  )
+                index++;
+            else {
+                int temp = 0;
+                temp = datos[index];
+                datos[index] = datos[index - 1];
+                datos[index - 1] = temp;
+                index--;
+            }
+        }
+        return;
+    }
     
 }
