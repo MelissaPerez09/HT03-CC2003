c6798b3ec387f4f5572c41e9ffb59f65d4e90e0e
MelissaPerez09 <per21385@uvg.edu.gt>
2022-02-24 22:40:06 -0600

    comas

--- Controlador.java
+++ Controlador.java
@@ -15,16 +15,16 @@ import Sorts.Selection;
 
 public class Controlador{
     public static void main(String[] args) {
-        int[] numbers = {10,24,30,87,2,3};
+        int[] numbers = {10,24,30,87,2,3,14};
 
         Gnome gnomesort = new Gnome();
         Quick quicksort= new Quick();
         Selection selectionsort = new Selection();
         Merge mergesort = new Merge();
 
-        gnomesort.gnomeSort( numbers,  6 );
-        quicksort.quicksort(numbers, 0, 5 );
+        gnomesort.gnomeSort( numbers,  7 );
+        quicksort.quicksort(numbers, 0, 6 );
         selectionsort.selectionSort(numbers, numbers.length);
-        mergesort.mergesort(numbers, 10, 30, 3);
+        mergesort.mergesort(numbers, 0, 3, 6);
     }
 }
